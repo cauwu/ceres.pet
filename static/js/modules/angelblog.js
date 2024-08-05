f82ab55f9c72667ba6496d0fa23e19ab2962cb57
ceres <69223874+cauwu@users.noreply@github.com>
2024-08-05 18:41:19 +0100

    added gallery and blog images

--- static/js/modules/angelblog.js
+++ static/js/modules/angelblog.js
@@ -1,6 +1,10 @@
 "use strict";
 
-window.onload = pfInsertEmail;
+window.addEventListener("load", (event) => {
+	pfInsertEmail();
+	pfModalSystem();
+})
+
 // insert the email as two seperate spans to deter scrapers
 function pfInsertEmail() {
 	document.getElementById("emailaddress").innerHTML = '<div id="addresslabel"><span>angel@</span>' + '<span>twnk.tv</span></div>';
@@ -17,4 +21,36 @@ function pfCopyEmail() {
 
 function pfHideNotif() {
 	document.getElementById("emailcopiednotif").style.display = "none";
+}
+
+// when an image is clicked on, display it in a large popup viewing area
+function pfModalSystem() {
+	var modal = document.getElementById("modal");
+	var modalContent = document.getElementById("modalcontent");
+	var modalCaption = document.getElementById("modalcaption");
+	
+	if (window.location.pathname === "/") {
+		
+	} else {
+		const imgs = document.querySelectorAll('figure img');
+		imgs.forEach(imgClicked);
+		function imgClicked(img,index) {
+			img.addEventListener('click',()=>{
+				modal.showModal();
+				modal.style.display = "flex";
+				modalContent.src = imgs[index].src;
+				modalCaption.innerHTML = imgs[index].alt;
+			});
+		};
+		
+		document.getElementById("modalclose").onclick = function() {
+			modal.style.display = "none";
+			modal.close();
+		}
+		
+		modal.onclick = function() {
+			modal.style.display = "none";
+			modal.close();
+		}
+	}
 }
