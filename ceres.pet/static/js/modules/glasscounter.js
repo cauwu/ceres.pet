759e568d68c7c27f22024226ad392e801a550ffe
cauwu <6922384+cauwu@users.noreply.github.com>
2025-11-04 16:33:32 +0000

    added vite

---
+++ ceres.pet/static/js/modules/glasscounter.js
@@ -0,0 +1,15 @@
+import { ref } from 'vue'
+export default {
+  setup() {
+    const count = ref(0)
+    return { count }
+  },
+  template: 
+  `<button @click="count--">
+    -1
+  </button>
+  <span>Count is: {{ count }}</span>
+  <button @click="count++">
+    +1
+  </button>`
+}
